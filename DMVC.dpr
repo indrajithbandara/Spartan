41538cd728b14a648fa5039c17ddffd55bb7992d
pauloab <abrahamkroenem@gmail.com>
2018-01-18 12:07:45 -0400

    Env file and version terminal interface

--- DMVC.dpr
+++ DMVC.dpr
@@ -4,11 +4,57 @@ program DMVC;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  System.StrUtils,
+  vEnv in 'Var\vEnv.pas';
+
+const
+  APP_NAME = 'DMVC';
+
+var
+  options_help: array [0 .. 0] of string = (
+    'Show framework version.'
+  );
+  options: array [0 .. 0] of string = (
+    '-v'
+  );
+  i: integer;
 
 begin
   try
+    case ParamCount of
+      0:
+        begin
+          Writeln(APP_NAME, ' Framework | A Delphi MVC framework to build desktop applications.');
+          Writeln('Authors: Paulo Barros <paulo.alfredo.barros@gmail.com>, Junior de Paula <juniiordepaula@gmail.com>');
+          Writeln('Version: ' + TEnv.System.version);
+          Writeln('');
+          Writeln('Usage: dmvc [option] <param|params>');
+          Writeln('');
+          Writeln('');
+          Writeln('Avaliable options:');
+          for i := 0 to High(options) do
+            Writeln('  ', options[i], '           ', options_help[i]);
+        end;
+      1:
+        begin
+          case ansiindexstr(ParamStr(1), options) of
+            0:
+              Writeln(APP_NAME, ' - ', TEnv.System.version);
+            1:
+              write('Help');
+          else
+            Writeln(format('Param "%s" not found', [ParamStr(1)]));
+          end;
+        end;
+    end;
+
   except
+    on e: exception do
+    begin
+      Writeln('Error starting ', APP_NAME, 'command line:');
+      Writeln(e.Message);
+    end;
   end;
 
 end.
---
+++ Var/vEnv.pas
@@ -0,0 +1,18 @@
+unit vEnv;
+
+interface
+
+type
+
+  TEnv = class
+  public Type
+    System = class
+    const
+      Version = 'v1.0.0';
+    end;
+
+  end;
+
+implementation
+
+end.
